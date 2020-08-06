ef09736179a1515151eb10a5e4df719a25c84a26
Vid Drobnic <vid.drobnic@gmail.com>
2020-08-06 14:31:18 +0200

    added attachment option

--- email.go
+++ email.go
@@ -1,36 +1,39 @@
 package ses_go
 
 import (
+	"bytes"
 	"github.com/aws/aws-sdk-go/service/ses"
+	"gopkg.in/mail.v2"
 )
 
-// Sends email with HTML content to recipient from sender email address. The email will have
-// specified subject. Content should be HTML text that is sent as the body of the email.
+// constructMail constructs new email object with specified data.
+func constructMail(sender, recipient, subject, bodyType, body string) *mail.Message {
+	msg := mail.NewMessage()
+	msg.SetHeader("From", sender)
+	msg.SetHeader("To", recipient)
+	msg.SetHeader("Subject", subject)
+	msg.SetBody(bodyType, body)
+
+	return msg
+}
+
+// sendMail sends message `msg`.
 // Returns AWS email ID and error. If email ID could not be got, empty string is returned.
-func SendHTMLEmail(sender, recipient, subject, htmlContent string) (string, error) {
+func sendMail(msg *mail.Message) (string, error) {
 	// Create SES object from session.
 	svc := ses.New(sess)
 
-	// Create input with specified parameters.
-	input := &ses.SendEmailInput{
-		Source: &sender,
-		Destination: &ses.Destination{
-			ToAddresses: []*string{&recipient},
-		},
-		Message: &ses.Message{
-			Body: &ses.Body{
-				Html: &ses.Content{
-					Data: &htmlContent,
-				},
-			},
-			Subject: &ses.Content{
-				Data: &subject,
-			},
-		},
+	// Convert message to bytes.
+	var emailRaw bytes.Buffer
+	_, _ = msg.WriteTo(&emailRaw)
+
+	// Create input instance for AWS.
+	input := &ses.SendRawEmailInput{
+		RawMessage: &ses.RawMessage{Data: emailRaw.Bytes()},
 	}
 
 	// Send email.
-	res, err := svc.SendEmail(input)
+	res, err := svc.SendRawEmail(input)
 
 	// Get AWS email ID.
 	awsID := ""
@@ -42,4 +45,26 @@ func SendHTMLEmail(sender, recipient, subject, htmlContent string) (string, erro
 	}
 
 	return awsID, err
+}
+
+// SendHTMLEmail sends email with HTML content to recipient from sender email address. The email will have
+// specified subject. Content should be HTML text that is sent as the body of the email.
+// Returns AWS email ID and error. If email ID could not be got, empty string is returned.
+func SendHTMLEmail(sender, recipient, subject, htmlContent string) (string, error) {
+	// Construct new message.
+	msg := constructMail(sender, recipient, subject, "text/html", htmlContent)
+
+	return sendMail(msg)
+}
+
+// SendHTMLEmailWithAttachment sends email with HTML content and attachment to recipient from sender email address.
+// The email will have  specified subject. Content should be HTML text that is sent as the body of the email.
+// `attachment` represents path to the attachment file.
+// Returns AWS email ID and error. If email ID could not be got, empty string is returned.
+func SendHTMLEmailWithAttachment(sender, recipient, subject, htmlContent, attachment string) (string, error) {
+	// Construct new message.
+	msg := constructMail(sender, recipient, subject, "text/html", htmlContent)
+	msg.Attach(attachment)
+
+	return sendMail(msg)
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module github.com/zerodays/ses-go
 
 go 1.14
 
-require github.com/aws/aws-sdk-go v1.33.14
+require (
+	github.com/aws/aws-sdk-go v1.33.14
+	gopkg.in/alexcesaro/quotedprintable.v3 v3.0.0-20150716171945-2caba252f4dc // indirect
+	gopkg.in/mail.v2 v2.3.1 // indirect
+)
